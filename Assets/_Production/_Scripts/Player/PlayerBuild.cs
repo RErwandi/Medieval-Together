fb2289596a48ac64a576cfffa66f678babce107e
Reynold Erwandi <reynolderwandi@gmail.com>
2019-09-08 21:28:53 +0700

    Import SO Architecture and Remove UniRx

---
+++ Assets/_Production/_Scripts/Player/PlayerBuild.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Reynold.Medieval
+{
+    public class PlayerBuild : MonoBehaviour
+    {
+        [SerializeField] private bool buildMode = false;
+        [SerializeField] private GameObject currentBuilding;
+        [SerializeField] [ReadOnly] private GameObject previewBuilding;
+
+        private void Start()
+        {
+            if (previewBuilding == null)
+            {
+                previewBuilding = new GameObject();
+            }
+            UpdatePreviewBuilding();
+        }
+
+        private void Update()
+        {
+            if (buildMode)
+            {
+                UpdatePositionPreviewBuilding();
+            }
+        }
+
+        public void ChangeBuilding(GameObject building)
+        {
+            currentBuilding = building;
+            DeleteAllChildPreviewBuilding();
+            UpdatePreviewBuilding();
+        }
+
+        void DeleteAllChildPreviewBuilding()
+        {
+            while(previewBuilding.transform.childCount > 0){ 
+                Destroy(previewBuilding.transform.GetChild(0).gameObject);  
+            }
+        }
+
+        void UpdatePreviewBuilding()
+        {
+            GameObject preview = Instantiate(currentBuilding, previewBuilding.transform);
+            preview.transform.localPosition = Vector3.zero;
+            preview.transform.localScale = Vector3.one;
+        }
+
+        void UpdatePositionPreviewBuilding()
+        {
+            previewBuilding.transform.position = GridManager.Instance.CurrentGrid.transform.position;
+        }
+    }
+}
